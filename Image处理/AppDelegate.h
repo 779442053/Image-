817a1e3474aa6adb6bd555183edc180f70dfd367
我就叫Hank <viphank@sina.com>
2016-04-10 20:26:22 +0800

    Initial Commit

---
+++ Image处理/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Image处理
+//
+//  Created by H on 16/4/10.
+//  Copyright © 2016年 TanZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Image处理/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Image处理
+//
+//  Created by H on 16/4/10.
+//  Copyright © 2016年 TanZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
